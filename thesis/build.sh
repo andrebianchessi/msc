eb0c6bfdc4f950824a755694f97a3297345b241e
andrebianchessi <andrebianchessi@gmail.com>
2022-06-24 01:13:07 -0300

    Basic pandoc-thesis simplification

--- thesis/build.sh
+++
@@ -1,6 +0,0 @@
-pandoc -o dist/thesis.pdf --from markdown --template eisvogel --listings -F pandoc-crossref --citeproc \
-config.md \
-introduction.md \
-objectives.md \
-exampleSection/exampleSection.md \
-references.md
