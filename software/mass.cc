ac5595d44a394ae9b61f311379393a42e913cbce
andrebianchessi <andrebianchessi@gmail.com>
2022-07-23 21:03:41 -0300

    Setup mass test

---
+++ software/mass.cc
@@ -0,0 +1,7 @@
+class Mass {
+    public:
+        double m; // mass
+        double x_0; // initial location
+        int xIndex; // index of x in the global system matrix
+        int xDotIndex; // index of xDot in the global system matrix
+};
