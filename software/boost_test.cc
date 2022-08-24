7a9dd598b272a7d30b63b2c6bd85cce4ba16e1ec
andrebianchessi <andrebianchessi@gmail.com>
2022-08-23 23:15:25 -0300

    Add boost array and vector tests

--- software/boost_test.cc
+++ software/boost_test.cc
@@ -1,7 +1,28 @@
-#include <boost/array.hpp>
+#include <gtest/gtest.h>
+#include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
 
-int main()
-{
-  boost::array<int, 3> a{};
-  return a[0];
+// Check that boost vectors and matrices are correctly imported
+
+using namespace boost::numeric::ublas;
+
+TEST(BoostTest, VectorTest){
+  vector<int> v(3);
+  v[0] = 1;
+  ASSERT_EQ(v[0],1);
+  ASSERT_EQ(v[1],0);
+  ASSERT_EQ(v[2],0);
+  ASSERT_EQ(v.size(),3);
+}
+
+TEST(BoostTest, MatrixTest){
+  matrix<int> m(2,2);
+  m(0,0) = 1;
+  m(0,1) = 2;
+  m(1,0) = 3;
+  m(1,1) = 4;
+  ASSERT_EQ(m(0,0),1);
+  ASSERT_EQ(m(1,1),4);
+  ASSERT_EQ(m.size1(),2);
+  ASSERT_EQ(m.size2(),2);
 }
