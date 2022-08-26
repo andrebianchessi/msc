fc83de791daeaca825fc1654e6c9944e92d253c6
andrebianchessi <andrebianchessi@gmail.com>
2022-08-26 03:27:26 -0300

    Add test based on natural frequency

--- software/problem_test.cc
+++ software/problem_test.cc
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "problem.h"
 #include <iostream>
-#include <boost/numeric/odeint.hpp>
+#include <math.h>
 
 TEST(ProblemTest, MassCreationTest) {
   Problem* p = new Problem();
@@ -323,19 +323,55 @@ TEST(ProblemTest,IntegrateStationaryTest) {
 }
 
 TEST(ProblemTest,IntegrateTest) {
+  // This test simulates a mass and spring system and automatically
+  // validates the answer by comparing the numerical and theoretical
+  // period of oscillation
+  //
+  // Expected natural frequency:
+  // 1/(2pi)*sqrt(k/M) -> 1/2pi
+  // Period of oscillation: 2pi
+  // Half period: pi
+  //
+  // The mass's speed starts off negative (it has a positive initial disp).
+  // Half period is how long it takes the mass to change direction, i.e.
+  // For the speed to become zero.
+
   Problem p = Problem();
   p.AddMass(1.0,0.0,0.0);
   p.AddMass(1.0,1.0,1.0);
   p.AddSpring(0,1,1.0);
   p.Build();
-
   p.FixMass(0);
-  p.SetInitialDisp(1, 1.0);
+  p.SetInitialDisp(1, 0.1);
 
-  p.Integrate(0.0, 10.0, 0.02);
+  p.Integrate(0.0, 4.0, 0.02);
   for (int i = 0; i < int(p.XHistory.size()); i++){
     ASSERT_DOUBLE_EQ(p.XHistory[i][p.GetMassDispIndex(0)],0.0);
     ASSERT_DOUBLE_EQ(p.XHistory[i][p.GetMassVelIndex(0)],0.0);
   }
-  p.PrintMassTimeHistory(1);
+  // Since mass starts with a positive initial displacement,
+  // its velocity after start will be negative
+  ASSERT_TRUE(p.XHistory[1][p.GetMassVelIndex(1)]<0);
+
+  // Find time in which speed becomes zero
+  int tNeg; // t in which velocity is negative
+  int tPos; // t in which velocity is positive
+  for (int i = 0; i < int(p.XHistory.size()) -1 ; i++){
+    if (p.XHistory[i][p.GetMassVelIndex(1)]<=0.0 && p.XHistory[i+1][p.GetMassVelIndex(1)]>0.0){
+      tNeg = i;
+      tPos = i+1;
+      break;
+    }
+  }
+  // Interpolate to find time in which velocity is zero
+  double x0 = p.t[tNeg];
+  double y0 = p.XHistory[tNeg][p.GetMassVelIndex(1)];
+  double x1 = p.t[tPos];
+  double y1 = p.XHistory[tPos][p.GetMassVelIndex(1)];
+  // (y1-y0)/(x1-x0) = (0-y0)/(x-x0)
+  // x = x0 -y0*(x1-x0)/(y1-y0)
+  double halfT = x0 - y0*(x1-x0)/(y1-y0);
+
+  double err = std::abs((halfT-M_PI)/M_PI);
+  ASSERT_TRUE(err <= 0.002); // error smaller than 0.2%
 }
