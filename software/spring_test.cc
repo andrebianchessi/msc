5e72ee7007081eb035ce703aba3c6378e77a6fb8
andrebianchessi <andrebianchessi@gmail.com>
2022-08-27 15:35:04 -0300

    Add spring errors test

--- software/spring_test.cc
+++ software/spring_test.cc
@@ -27,3 +27,18 @@ TEST(SpringTest, SimpleTest) {
     ASSERT_DOUBLE_EQ(K(1,0),30.0);
     ASSERT_DOUBLE_EQ(K(1,1),-30.0);
 }
+
+TEST(SpringTest, ErrorsTest) {
+    Problem* p = new Problem();
+    p->AddMass(10.0,0.0,0.0);
+    p->AddMass(10.0,1.0,1.0);
+
+    auto e = p->AddSpring(0,0,30.0);
+    ASSERT_TRUE(e.isError);
+
+    e = p->AddSpring(0,99,30.0);
+    ASSERT_TRUE(e.isError);
+
+    e = p->AddSpring(99,0,30.0);
+    ASSERT_TRUE(e.isError);
+}
