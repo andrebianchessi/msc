b23422fd22c4d03dda8196805585fc90f973a7b3
andrebianchessi <andrebianchessi@gmail.com>
2022-09-05 23:02:00 -0300

    Format

--- software/mass.h
+++ software/mass.h
@@ -3,18 +3,18 @@
 class Mass {
     friend class Problem;
 
-    public:
-        double m; // mass value
-        double px; // horizontal position
-        double py; // vertical position
-        int xIndex; // global index of horizontal degree of freedom
+   public:
+    double m;    // mass value
+    double px;   // horizontal position
+    double py;   // vertical position
+    int xIndex;  // global index of horizontal degree of freedom
 
-    private:
-        // Mass instances can only be constructed by Problem instances
-        Mass(double m, double px, double py, int xIndex){
-            this->m = m;
-            this->px = px;
-            this->py = py;
-            this->xIndex = xIndex;
-        }
+   private:
+    // Mass instances can only be constructed by Problem instances
+    Mass(double m, double px, double py, int xIndex) {
+        this->m = m;
+        this->px = px;
+        this->py = py;
+        this->xIndex = xIndex;
+    }
 };
