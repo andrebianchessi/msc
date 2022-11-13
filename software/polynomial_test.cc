658f11b66e8ffe24fef6c1362fde09b563a51f92
andrebianchessi <andrebianchessi@gmail.com>
2022-11-13 12:03:15 -0300

    PolynomialTest refactor

--- software/polynomial_test.cc
+++ software/polynomial_test.cc
@@ -4,7 +4,7 @@
 
 #include "maybe.h"
 
-TEST(PolyTest, NodeTest) {
+TEST(NodeTest, AddChildTest) {
     // P = x^2 + xy + x + y^2 + y + 1
     auto root = Node(3);
     ASSERT_EQ(root.children.size(), 3);
@@ -49,7 +49,7 @@ TEST(PolyTest, NodeTest) {
     ASSERT_EQ(root.children[2]->children[2]->exp, 0);
 }
 
-TEST(PolyTest, constructorTest) {
+TEST(PolyConstructorTest, constructorTest) {
     // Invalid values
     Maybe<Poly> r = Poly::NewPoly(-1, 0);
     ASSERT_TRUE(r.isError);
@@ -100,55 +100,64 @@ TEST(PolyTest, constructorTest) {
     ASSERT_EQ(p.coefficients->children[2]->children[2]->children.size(), 0);
 }
 
-TEST(PolyTest, dfsTest) {
+class PolyTest : public testing::Test {
+   public:
+    // Polynomial of two vars or order two with all coeff. = 0
+    // P = 0*x^2 + 0*xy + 0*x + 0*y^2 + 0*y + 0
+    Poly n2o2Zeros;
+    // Polynomial of two vars or order two with all coeff. = 2.0
+    // P = 2*x^2 + 2*xy + 2*x + 2*y^2 + 2*y + 2
+    Poly n2o2Twos;
+
+    // Polynomial of two vars or order two
+    // P = 1x^2 + 2xy + 3x + 4y^2 + 5y + 6
+    Poly n2o2;
+
+    void SetUp() {
+        // Called before every TEST_F
+        Maybe<Poly> r = Poly::NewPoly(2, 2);
+        ASSERT_FALSE(r.isError);
+        Poly p = r.val;
+        this->n2o2Zeros = p;
+
+        r = Poly::NewPoly(2, 2, 2.0);
+        ASSERT_FALSE(r.isError);
+        p = r.val;
+        this->n2o2Twos = p;
+
+        r = Poly::NewPoly(2, 2);
+        ASSERT_FALSE(r.isError);
+        p = r.val;
+        p.coefficients->children[0]->children[0]->a = 1.0;
+        p.coefficients->children[1]->children[0]->a = 2.0;
+        p.coefficients->children[1]->children[1]->a = 3.0;
+        p.coefficients->children[2]->children[0]->a = 4.0;
+        p.coefficients->children[2]->children[1]->a = 5.0;
+        p.coefficients->children[2]->children[2]->a = 6.0;
+        this->n2o2 = p;
+    }
+};
+
+TEST_F(PolyTest, dfsTest) {
+    double a = 2.0;  // coefficients
     double x = 2.0;
     double y = 3.0;
-    double a = 5.0;
-
-    Maybe<Poly> m = Poly::NewPoly(2, 2, a);
-    ASSERT_FALSE(m.isError);
-    Poly p = m.val;
-
     std::vector<double> X = {x, y};
-    double a_y_0 = dfs(p.coefficients->children[0]->children[0], 2, &X);
+
+    double a_y_0 =
+        dfs(this->n2o2Twos.coefficients->children[0]->children[0], 2, &X);
     ASSERT_DOUBLE_EQ(a_y_0, a * 1);
-    double x_2_a_y_0 = dfs(p.coefficients->children[0], 1, &X);
+    double x_2_a_y_0 = dfs(this->n2o2Twos.coefficients->children[0], 1, &X);
     ASSERT_DOUBLE_EQ(x_2_a_y_0, x * x * a * 1);
 }
 
-TEST(PolyTest, operatorTest) {
-    Maybe<Poly> m = Poly::NewPoly(2, 2);
-    ASSERT_FALSE(m.isError);
-    Poly p = m.val;
-
-    std::vector<double> X = {1.0, 2.0};
-    auto v = p(&X);
-    ASSERT_FALSE(v.isError);
-    ASSERT_DOUBLE_EQ(v.val, 0.0);
-
-    // x1^2*x2^0
-    p.coefficients->children[0]->children[0]->a = 1.0;
-
-    // x1^1*x2^1
-    p.coefficients->children[1]->children[0]->a = 2.0;
-
-    // x1^1*x2^0
-    p.coefficients->children[1]->children[1]->a = 3.0;
-
-    // x1^0*x2^2
-    p.coefficients->children[2]->children[0]->a = 4.0;
-
-    // x1^0*x2^1
-    p.coefficients->children[2]->children[1]->a = 5.0;
-
-    // x1^0*x2^0
-    p.coefficients->children[2]->children[2]->a = 6.0;
-
+TEST_F(PolyTest, operatorTest) {
+    std::vector<double> X;
     double x1 = 9.0;
     double x2 = 17.0;
     X = {x1, x2};
 
-    v = p(&X);
+    auto v = n2o2(&X);
     ASSERT_FALSE(v.isError);
 
     double expected = 1.0 * x1 * x1 + 2.0 * x1 * x2 + 3.0 * x1 + 4.0 * x2 * x2 +
@@ -156,99 +165,57 @@ TEST(PolyTest, operatorTest) {
     ASSERT_DOUBLE_EQ(v.val, expected);
 }
 
-TEST(PolyTest, DxiTest) {
-    Maybe<Poly> m = Poly::NewPoly(2, 2);
-    ASSERT_FALSE(m.isError);
-    Poly p = m.val;
-
+TEST_F(PolyTest, DxiTest) {
     std::vector<double> X = {1.0, 2.0};
-    auto v = p.Dxi(0, &X);
-    ASSERT_FALSE(v.isError);
-    ASSERT_DOUBLE_EQ(v.val, 0.0);
-    v = p.Dxi(1, &X);
+    auto v = n2o2Zeros.Dxi(0, &X);
     ASSERT_FALSE(v.isError);
     ASSERT_DOUBLE_EQ(v.val, 0.0);
 
-    ASSERT_TRUE(p.Dxi(2, &X).isError);
-    ASSERT_TRUE(p.Dxi(-1, &X).isError);
+    v = n2o2Zeros.Dxi(1, &X);
+    ASSERT_FALSE(v.isError);
+    ASSERT_DOUBLE_EQ(v.val, 0.0);
 
-    // x1^2*x2^0
-    p.coefficients->children[0]->children[0]->a = 1.0;
-
-    // x1^1*x2^1
-    p.coefficients->children[1]->children[0]->a = 2.0;
-
-    // x1^1*x2^0
-    p.coefficients->children[1]->children[1]->a = 3.0;
-
-    // x1^0*x2^2
-    p.coefficients->children[2]->children[0]->a = 4.0;
-
-    // x1^0*x2^1
-    p.coefficients->children[2]->children[1]->a = 5.0;
-
-    // x1^0*x2^0
-    p.coefficients->children[2]->children[2]->a = 6.0;
+    ASSERT_TRUE(n2o2Zeros.Dxi(2, &X).isError);
+    ASSERT_TRUE(n2o2Zeros.Dxi(-1, &X).isError);
 
     double x1 = 9.0;
     double x2 = 17.0;
     X = {x1, x2};
 
-    v = p.Dxi(0, &X);
+    v = n2o2.Dxi(0, &X);
     ASSERT_FALSE(v.isError);
     double d1Expected = 1.0 * 2 * x1 + 2.0 * x2 + 3.0;
     ASSERT_DOUBLE_EQ(v.val, d1Expected);
 
-    v = p.Dxi(1, &X);
+    v = n2o2.Dxi(1, &X);
     ASSERT_FALSE(v.isError);
     double d2Expected = 2.0 * x1 + 4.0 * 2 * x2 + 5.0;
     ASSERT_DOUBLE_EQ(v.val, d2Expected);
 }
 
-TEST(PolyTest, DDxiTest) {
-    Maybe<Poly> m = Poly::NewPoly(2, 2);
-    ASSERT_FALSE(m.isError);
-    Poly p = m.val;
-
+TEST_F(PolyTest, DDxiTest) {
     std::vector<double> X = {1.0, 2.0};
-    auto v = p.DDxi(0, &X);
+
+    auto v = n2o2Zeros.DDxi(0, &X);
     ASSERT_FALSE(v.isError);
     ASSERT_DOUBLE_EQ(v.val, 0.0);
-    v = p.DDxi(1, &X);
+    v = n2o2Zeros.DDxi(1, &X);
     ASSERT_FALSE(v.isError);
     ASSERT_DOUBLE_EQ(v.val, 0.0);
 
-    ASSERT_TRUE(p.DDxi(2, &X).isError);
-    ASSERT_TRUE(p.DDxi(-1, &X).isError);
-
-    // x1^2*x2^0
-    p.coefficients->children[0]->children[0]->a = 1.0;
-
-    // x1^1*x2^1
-    p.coefficients->children[1]->children[0]->a = 2.0;
-
-    // x1^1*x2^0
-    p.coefficients->children[1]->children[1]->a = 3.0;
-
-    // x1^0*x2^2
-    p.coefficients->children[2]->children[0]->a = 4.0;
-
-    // x1^0*x2^1
-    p.coefficients->children[2]->children[1]->a = 5.0;
-
-    // x1^0*x2^0
-    p.coefficients->children[2]->children[2]->a = 6.0;
+    ASSERT_TRUE(n2o2Zeros.DDxi(2, &X).isError);
+    ASSERT_TRUE(n2o2Zeros.DDxi(-1, &X).isError);
 
     double x1 = 9.0;
     double x2 = 17.0;
     X = {x1, x2};
 
-    v = p.DDxi(0, &X);
+    v = n2o2.DDxi(0, &X);
     ASSERT_FALSE(v.isError);
     double dd1Expected = 1.0 * 2;
     ASSERT_DOUBLE_EQ(v.val, dd1Expected);
 
-    v = p.DDxi(1, &X);
+    v = n2o2.DDxi(1, &X);
     ASSERT_FALSE(v.isError);
     double dd2Expected = 4.0 * 2;
     ASSERT_DOUBLE_EQ(v.val, dd2Expected);
