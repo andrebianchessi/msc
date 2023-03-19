ebd5bcaa54707187e6f613fd0ed30c7b4b73074f
andrebianchessi <andrebianchessi@gmail.com>
2023-03-19 18:16:44 -0300

    Clean up Pimodel Train tests

--- software/pimodel_test.cc
+++ software/pimodel_test.cc
@@ -950,7 +950,6 @@ class PimodelTrainTest : public testing::Test {
         this->cMax = 0.2;
         this->tMin = 0.0;
         this->tMax = 1.0;
-        this->initialDisp = 1.0;
         this->timeDiscretization = 2;
         this->kcDiscretization = 1;
         this->order = 3;
@@ -962,7 +961,6 @@ class PimodelTrainTest : public testing::Test {
         this->pd.AddSpring(0, 1, kMin, kMax);
         this->pd.AddDamper(0, 1, cMin, cMax);
         this->pd.SetFixedMass(0);
-        this->pd.AddInitialDisp(1, initialDisp);
     }
 
     void Train() {
@@ -1013,7 +1011,24 @@ class PimodelTrainTest : public testing::Test {
     }
 };
 
-TEST_F(PimodelTrainTest, SimpleTrainTest) {
+TEST_F(PimodelTrainTest, InitialDispTrainTest) {
+    this->log = false;
+    this->pd.AddInitialDisp(1, 1.0);
+    this->Train();
+    this->Plot();
+}
+
+TEST_F(PimodelTrainTest, InitialVelTrainTest) {
+    this->log = false;
+    this->pd.AddInitialVel(1, 1.0);
+    this->Train();
+    this->Plot();
+}
+
+TEST_F(PimodelTrainTest, InitialDispAndVelTrainTest) {
+    this->log = false;
+    this->pd.AddInitialDisp(1, 1.0);
+    this->pd.AddInitialVel(1, 1.0);
     this->Train();
     this->Plot();
 }
@@ -1339,57 +1354,57 @@ TEST_F(PimodelsTest, OperatorTest) {
     ASSERT_DOUBLE_EQ(eval.val[1], f[0] * t + f[1] * kVal + f[2] * cVal + f[3]);
 }
 
-TEST(PimodelsTrainingTest, TrainTest) {
-    double mass = 1;
-    double kMin = 0.8;
-    double kMax = 1.0;
-    double cMin = 0.0;
-    double cMax = 0.2;
-    double tMax = 2.0;
-    double initialDisp = 1.0;
+// TEST(PimodelsTrainingTest, TrainTest) {
+//     double mass = 1;
+//     double kMin = 0.8;
+//     double kMax = 1.0;
+//     double cMin = 0.0;
+//     double cMax = 0.2;
+//     double tMax = 2.0;
+//     double initialDisp = 1.0;
 
-    auto pd = ProblemDescription();
-    pd.AddMass(mass, 0.0, 0.0);
-    pd.AddMass(mass, 1.0, 0.0);
-    pd.AddSpring(0, 1, kMin, kMax);
-    pd.AddDamper(0, 1, cMin, cMax);
-    pd.SetFixedMass(0);
-    pd.AddInitialDisp(1, initialDisp);
+//     auto pd = ProblemDescription();
+//     pd.AddMass(mass, 0.0, 0.0);
+//     pd.AddMass(mass, 1.0, 0.0);
+//     pd.AddSpring(0, 1, kMin, kMax);
+//     pd.AddDamper(0, 1, cMin, cMax);
+//     pd.SetFixedMass(0);
+//     pd.AddInitialDisp(1, initialDisp);
 
-    int nModels = 2;
-    int timeDiscretization = 2;
-    int kcDiscretization = 1;
-    int order = 2;
-    double learningRate = 0.0001;
-    int maxSteps = 10;
-    bool log = true;
+//     int nModels = 4;
+//     int timeDiscretization = 2;
+//     int kcDiscretization = 1;
+//     int order = 3;
+//     double learningRate = 0.01;
+//     int maxSteps = 500;
+//     bool log = true;
 
-    // Train all models
-    Pimodels models = Pimodels(pd, tMax, nModels, timeDiscretization,
-                               kcDiscretization, order);
-    models.Train(learningRate, maxSteps, log);
+//     // Train all models
+//     Pimodels models = Pimodels(pd, tMax, nModels, timeDiscretization,
+//                                kcDiscretization, order);
+//     models.Train(learningRate, maxSteps, log);
 
-    // Get problem using intermediate value for k and c, and integrate it.
-    // Then, compare the model's prediction with the problem's result.
-    double k = (kMin + kMax) / 2;
-    double c = (cMin + cMax) / 2;
-    Maybe<Problem> mP = pd.BuildFromVector(std::vector<double>{k, c});
-    ASSERT_FALSE(mP.isError);
-    Problem p = mP.val;
-    p.Integrate(tMax);
+//     // Get problem using intermediate value for k and c, and integrate it.
+//     // Then, compare the model's prediction with the problem's result.
+//     double k = (kMin + kMax) / 2;
+//     double c = (cMin + cMax) / 2;
+//     Maybe<Problem> mP = pd.BuildFromVector(std::vector<double>{k, c});
+//     ASSERT_FALSE(mP.isError);
+//     Problem p = mP.val;
+//     p.Integrate(tMax);
 
-    std::vector<double> tkc = std::vector<double>{0.0, k, c};
-    Maybe<std::vector<double>> X;
-    std::cout << "t,x0,modelX0,x1,modelX1" << std::endl;
-    for (int i = 0; i < int(p.t.size()); i += 1) {
-        tkc[0] = p.t[i];
-        X = models(&tkc);
-        ASSERT_FALSE(X.isError);
+//     std::vector<double> tkc = std::vector<double>{0.0, k, c};
+//     Maybe<std::vector<double>> X;
+//     std::cout << "t,x0,modelX0,x1,modelX1" << std::endl;
+//     for (int i = 0; i < int(p.t.size()); i += 1) {
+//         tkc[0] = p.t[i];
+//         X = models(&tkc);
+//         ASSERT_FALSE(X.isError);
 
-        std::cout << p.t[i] << ",";
-        std::cout << p.XHistory[i][0] << ",";
-        std::cout << X.val[0] << ",";
-        std::cout << p.XHistory[i][1] << ",";
-        std::cout << X.val[1] << std::endl;
-    }
-}
+//         std::cout << p.t[i] << ",";
+//         std::cout << p.XHistory[i][0] << ",";
+//         std::cout << X.val[0] << ",";
+//         std::cout << p.XHistory[i][1] << ",";
+//         std::cout << X.val[1] << std::endl;
+//     }
+// }
